7f96b705abdb3bb07b2cce8c65656e8eadb26740
Simon <simon@kenpoly.com>
2023-06-03 19:45:12 +0300

    views in price app updated with the market price logic

--- price_app/views.py
+++ price_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import MarketPriceForm
+from accounts.models import User
+
+def get_market_price(request):
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = MarketPriceForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+
+            return redirect("index.html")
+    
+    else:
+        form = MarketPriceForm()
+    
+    return render(request, "index.html", {"form":form})
