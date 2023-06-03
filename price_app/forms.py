b5cc0d8a351a5b3c0f4bda815902e47e4efdf3d2
Simon <simon@kenpoly.com>
2023-06-03 19:27:02 +0300

    forms file created an updated

---
+++ price_app/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+
+class MarketPriceForm(forms.Form):
+    customer_name = forms.CharField(label="Customer Name", required=True)
+    customer_branch = forms.CharField(label="Customer Branch", required=True)
+    kenpoly_product_name = forms.CharField(label="Kenpoly Product Name", required=True)
+    kenpoly_price = forms.CharField(label="Kenpoly Price", required=True)
+    competitor_name = forms.CharField(label="Competitor's Name", required=True)
+    competitor_product_name = forms.CharField(label="Competitor's Product", required=True)
+    competitor_price = forms.CharField(label="Competitor Price", required=True)
