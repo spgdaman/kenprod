00318aac84431649e0bcadf8490812ad1f073363
Simon Waigi <simon@KenpolyDomain.com>
2023-06-03 16:40:17 +0300

    forms file created

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name',)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        models = User
+        fields = ('email', 'first_name', 'last_name',)
