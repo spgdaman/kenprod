29f05012e50ef23593786479236978a4aba4f731
Simon Waigi <simon@KenpolyDomain.com>
2023-06-03 19:10:20 +0300

    market price model created

--- price_app/models.py
+++ price_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class MarketPrice(models.Model):
+    sales_person = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    customer_name = models.CharField(max_length=100)
+    customer_branch= models.CharField(max_length=100)
+    kenpoly_product_name = models.CharField(max_length=100)
+    kenpoly_price = models.IntegerField(blank=False, null=True)
+    competitor_name = models.CharField(max_length=100)
+    competitor_product_name = models.CharField(max_length=100)
+    competitor_price = models.IntegerField(blank=False, null=True)
