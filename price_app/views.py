cea1f858a702f86b65afa814e014ddc716975e83
Simon <simon@kenpoly.com>
2023-06-04 14:00:44 +0300

    views updated to save data to market price model

--- price_app/views.py
+++ price_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 
 from .forms import MarketPriceForm
 from accounts.models import User
+from .models import MarketPrice
 
 def get_market_price(request):
     if request.method == "POST":
@@ -12,7 +13,28 @@ def get_market_price(request):
         if form.is_valid():
             data = form.save(commit=False)
             data.user = request.user
-            data.save()
+            # data.save()
+
+            # sales_person = User.objects.get()
+
+            customer_name = request.POST['customer_name']
+            customer_branch = request.POST['customer_branch']
+            kenpoly_product_name = request.POST['kenpoly_product_name']
+            kenpoly_price = request.POST['kenpoly_price']
+            competitor_name = request.POST['competitor_name']
+            competitor_product_name = request.POST['competitor_product_name']
+            competitor_price = request.POST['competitor_price']
+
+            market_data = MarketPrice.objects.create(
+                sales_person = request.user,
+                customer_name = customer_name,
+                customer_branch = customer_branch,
+                kenpoly_product_name = kenpoly_product_name,
+                kenpoly_price = kenpoly_price,
+                competitor_name = competitor_name,
+                competitor_product_name = competitor_product_name,
+                competitor_price = competitor_price
+            )
             return redirect("price_capture.html")
     
     else:
