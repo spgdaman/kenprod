5bd2c00e89e882ecf5d44317ca8b02d46f89baea
Simon <simon@kenpoly.com>
2023-06-14 15:53:14 +0300

    back to the drawing board

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,27 +1,27 @@
 from django.contrib import admin
 # from .models import FinishedGood, Composition, RawMaterial,VariableCost,Packing,
-from .models import (
-    Test, 
-    FinishedGood, 
-    Composition, 
-    CompositionLineItem,
-    RawMaterialLineItem,
-    RawMaterial,
-    RawMaterialCategory,
-    Attribute,
-    Recipe,
-    SemiFinishedGood
-)
+# from .models import (
+#     Test, 
+#     FinishedGood, 
+#     Composition, 
+#     CompositionLineItem,
+#     RawMaterialLineItem,
+#     RawMaterial,
+#     RawMaterialCategory,
+#     Attribute,
+#     Recipe,
+#     SemiFinishedGood
+# )
 
-admin.site.register(FinishedGood)
-admin.site.register(Composition)
-admin.site.register(CompositionLineItem)
-admin.site.register(RawMaterialLineItem)
-admin.site.register(RawMaterial)
-admin.site.register(RawMaterialCategory)
-admin.site.register(Attribute)
-admin.site.register(Recipe)
-admin.site.register(SemiFinishedGood)
+# admin.site.register(FinishedGood)
+# admin.site.register(Composition)
+# admin.site.register(CompositionLineItem)
+# admin.site.register(RawMaterialLineItem)
+# admin.site.register(RawMaterial)
+# admin.site.register(RawMaterialCategory)
+# admin.site.register(Attribute)
+# admin.site.register(Recipe)
+# admin.site.register(SemiFinishedGood)
 # admin.site.register(VariableCost)
 # admin.site.register(Packing)
-admin.site.register(Test)
+# admin.site.register(Test)
--- cogs/models.py
+++ cogs/models.py
@@ -1,109 +1,109 @@
 from django.db import models
 from django.conf import settings
 
-class Attribute(models.Model):
-    attribute_name = models.CharField(max_length=30, blank=True)
+# class Attribute(models.Model):
+#     attribute_name = models.CharField(max_length=30, blank=True)
 
-    def __str__(self):
-        return self.attribute_name
+#     def __str__(self):
+#         return self.attribute_name
 
-class Recipe(models.Model):
-    recipe_name = models.CharField(max_length=30, blank=True)
+# class Recipe(models.Model):
+#     recipe_name = models.CharField(max_length=30, blank=True)
 
-    def __str__(self):
-        return self.recipe_name
+#     def __str__(self):
+#         return self.recipe_name
 
-class FinishedGood(models.Model):
-    name = models.CharField(max_length=50, blank=True)
-    attribute = models.ForeignKey(Attribute, models.DO_NOTHING, blank=True, null=True)
-    weight = models.DecimalField(blank=True, max_digits=50, decimal_places=4)
-    recipe = models.ForeignKey(Recipe, models.DO_NOTHING, blank=True, null=True)
-    selling_price = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
+# class FinishedGood(models.Model):
+#     name = models.CharField(max_length=50, blank=True)
+#     attribute = models.ForeignKey(Attribute, models.DO_NOTHING, blank=True, null=True)
+#     weight = models.DecimalField(blank=True, max_digits=50, decimal_places=4)
+#     recipe = models.ForeignKey(Recipe, models.DO_NOTHING, blank=True, null=True)
+#     selling_price = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
+#     is_active = models.BooleanField(default=True)
+#     created_at = models.DateTimeField(auto_now=True)
+#     updated_at = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-class RawMaterialCategory(models.Model):
-    category = models.CharField(max_length=50, blank=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
+# class RawMaterialCategory(models.Model):
+#     category = models.CharField(max_length=50, blank=True)
+#     created_at = models.DateTimeField(auto_now=True)
+#     updated_at = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
-        return self.category
+#     def __str__(self):
+#         return self.category
 
-class RawMaterialLineItem(models.Model):
-    # item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
-    material = models.CharField(max_length=50, blank=True)
-    category = models.ForeignKey(RawMaterialCategory, models.DO_NOTHING, blank=True, null=True)
-    rm_cost = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
-    landing_cost = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
+# class RawMaterialLineItem(models.Model):
+#     # item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
+#     material = models.CharField(max_length=50, blank=True)
+#     category = models.ForeignKey(RawMaterialCategory, models.DO_NOTHING, blank=True, null=True)
+#     rm_cost = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
+#     landing_cost = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
+#     created_at = models.DateTimeField(auto_now=True)
+#     updated_at = models.DateTimeField(null=True, blank=True)
     
-    def landed_cost_per_kilo(self):
-        '''returns the landing cost per kg'''
-        if self.rm_cost < 0:
-            return (-1*self.rm_cost)*(1+(self.landing_cost/100))
-        else:
-            return self.rm_cost *(1+(self.landing_cost/100))
+#     def landed_cost_per_kilo(self):
+#         '''returns the landing cost per kg'''
+#         if self.rm_cost < 0:
+#             return (-1*self.rm_cost)*(1+(self.landing_cost/100))
+#         else:
+#             return self.rm_cost *(1+(self.landing_cost/100))
         
-    landed_cost_per_kg = property(landed_cost_per_kilo)
+#     landed_cost_per_kg = property(landed_cost_per_kilo)
 
-    def __str__(self):
-        return self.material
+#     def __str__(self):
+#         return self.material
 
-class RawMaterial(models.Model):
-    item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
-    raw_material = models.ForeignKey(RawMaterialLineItem, models.DO_NOTHING, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
+# class RawMaterial(models.Model):
+#     item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
+#     raw_material = models.ForeignKey(RawMaterialLineItem, models.DO_NOTHING, blank=True, null=True)
+#     created_at = models.DateTimeField(auto_now=True)
+#     updated_at = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.raw_material} material used for --> product {self.item_name}"
+#     def __str__(self):
+#         return f"{self.raw_material} material used for --> product {self.item_name}"
 
-class CompositionLineItem(models.Model):
-    composite_material = models.CharField(max_length=30, blank=True)
+# class CompositionLineItem(models.Model):
+#     composite_material = models.CharField(max_length=30, blank=True)
 
-    def __str__(self):
-        return self.composite_material
+#     def __str__(self):
+#         return self.composite_material
 
-class Composition(models.Model):
-    item_name = models.ForeignKey(RawMaterial, models.DO_NOTHING, blank=True, null=True)
-    composition = models.ForeignKey(CompositionLineItem, models.DO_NOTHING, blank=True, null=True)
-    ratio = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
-    price_per_kg = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(null=True, blank=True)
+# class Composition(models.Model):
+#     item_name = models.ForeignKey(RawMaterial, models.DO_NOTHING, blank=True, null=True)
+#     composition = models.ForeignKey(CompositionLineItem, models.DO_NOTHING, blank=True, null=True)
+#     ratio = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
+#     price_per_kg = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
+#     created_at = models.DateTimeField(auto_now=True)
+#     updated_at = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
-        return f"{self.composition} composition for --> {self.item_name}"
+#     def __str__(self):
+#         return f"{self.composition} composition for --> {self.item_name}"
 
-    def get_price_for_ratio(self):
-        '''returns the price for ratio'''
-        if self.ratio < 0:
-            return (self.price_per_kg * (-1*self.ratio)) * 100
-        else:
-            return (self.price_per_kg * self.ratio) * 100
-    price_for_ratio = property(get_price_for_ratio)
+#     def get_price_for_ratio(self):
+#         '''returns the price for ratio'''
+#         if self.ratio < 0:
+#             return (self.price_per_kg * (-1*self.ratio)) * 100
+#         else:
+#             return (self.price_per_kg * self.ratio) * 100
+#     price_for_ratio = property(get_price_for_ratio)
 
-class SemiFinishedGood(models.Model):
-    item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
-    sfg_name = models.CharField(max_length=50, blank=True)
-    weight = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
-    attribute = models.ForeignKey(Attribute, models.DO_NOTHING, blank=True, null=True)
-    component_quantity = models.IntegerField()
-    composition = models.ForeignKey(Composition, models.DO_NOTHING, blank=True, null=True)
-    # recipe = models.ForeignKey(Recipe, models.DO_NOTHING, blank=True, null=True)
+# class SemiFinishedGood(models.Model):
+#     item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
+#     sfg_name = models.CharField(max_length=50, blank=True)
+#     weight = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+#     attribute = models.ForeignKey(Attribute, models.DO_NOTHING, blank=True, null=True)
+#     component_quantity = models.IntegerField()
+#     composition = models.ForeignKey(Composition, models.DO_NOTHING, blank=True, null=True)
+#     # recipe = models.ForeignKey(Recipe, models.DO_NOTHING, blank=True, null=True)
 
 
-class ExternalComponent(models.Model):
-    pass
+# class ExternalComponent(models.Model):
+#     pass
 
-class Packaging(models.Model):
-    pass
+# class Packaging(models.Model):
+#     pass
 
 # class VariableCost(models.Model):
 #     # item_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
