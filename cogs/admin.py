474473e1cae32dd8b1d7c53d15c4474e2cc89771
Simon <simon@kenpoly.com>
2023-06-15 16:42:50 +0300

    refinement of models in the cogs app

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ExchangeRate,Labeling,RawMaterial,RawMaterialLineItem,RawMaterialCategory,FinishedGood,SemiFinishedGood,Foiling,Packing,Power,Labour
+from .models import ExchangeRate,Labeling,RawMaterial,RawMaterialLineItem,RawMaterialCategory,FinishedGood,SemiFinishedGood,Foiling,Packing,Power,Labour,ExternalComponent,ExternalComponentLineItem,ExternalComponentName
 
 admin.site.register(ExchangeRate)
 admin.site.register(Labeling)
@@ -12,6 +12,9 @@ admin.site.register(Packing)
 admin.site.register(Power)
 admin.site.register(Labour)
 admin.site.register(RawMaterialCategory)
+admin.site.register(ExternalComponent)
+admin.site.register(ExternalComponentLineItem)
+admin.site.register(ExternalComponentName)
 
 # from .models import FinishedGood, Composition, RawMaterial,VariableCost,Packing,
 # from .models import (
--- cogs/models.py
+++ cogs/models.py
@@ -10,7 +10,8 @@ class ExchangeRate(models.Model):
 class FinishedGood(models.Model):
     name = models.CharField(max_length=50, blank=True)
     unit = models.DecimalField(blank=True, max_digits=50, decimal_places=4)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=4)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -20,7 +21,7 @@ class SemiFinishedGood(models.Model):
     name = models.CharField(max_length=50, blank=True)
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
     unit = models.DecimalField(blank=True, max_digits=50, decimal_places=4)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -36,11 +37,12 @@ class RawMaterialCategory(models.Model):
 class RawMaterialLineItem(models.Model):
     name = models.ForeignKey(RawMaterialCategory, models.DO_NOTHING, blank=True, null=True)
     unit = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name.category
 
 class RawMaterial(models.Model):
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
@@ -51,13 +53,40 @@ class RawMaterial(models.Model):
 
     def __str__(self):
         return f"{self.raw_material} material used for --> product {self.fg_name}"
+    
+class ExternalComponentName(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+class ExternalComponentLineItem(models.Model):
+    name = models.ForeignKey(ExternalComponentName, models.DO_NOTHING, blank=True, null=True)
+    unit = models.DecimalField(blank=True, max_digits=50, decimal_places=2)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name.name
+    
+class ExternalComponent(models.Model):
+    fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
+    sfg_name = models.ForeignKey(SemiFinishedGood, models.DO_NOTHING, blank=True, null=True)
+    component = models.ForeignKey(ExternalComponentLineItem, models.DO_NOTHING, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.component.name}"
 
 class Labeling(models.Model):
     description = models.CharField(max_length=50, blank=True)
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
     sfg_name = models.ForeignKey(SemiFinishedGood, models.DO_NOTHING, blank=True, null=True)
     unit = models.IntegerField(null=True, blank=True)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -68,7 +97,8 @@ class Foiling(models.Model):
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
     sfg_name = models.ForeignKey(SemiFinishedGood, models.DO_NOTHING, blank=True, null=True)
     unit = models.IntegerField(null=True, blank=True)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -79,7 +109,8 @@ class Packing(models.Model):
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
     sfg_name = models.ForeignKey(SemiFinishedGood, models.DO_NOTHING, blank=True, null=True)
     unit = models.IntegerField(null=True, blank=True)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -90,7 +121,8 @@ class Power(models.Model):
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
     sfg_name = models.ForeignKey(SemiFinishedGood, models.DO_NOTHING, blank=True, null=True)
     unit = models.IntegerField(null=True, blank=True)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -101,7 +133,8 @@ class Labour(models.Model):
     fg_name = models.ForeignKey(FinishedGood, models.DO_NOTHING, blank=True, null=True)
     sfg_name = models.ForeignKey(SemiFinishedGood, models.DO_NOTHING, blank=True, null=True)
     unit = models.IntegerField(null=True, blank=True)
-    rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
+    cost_per_unit = models.DecimalField(blank=True, max_digits=10, decimal_places=2)
+    # rate = models.ForeignKey(ExchangeRate, models.DO_NOTHING, blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
