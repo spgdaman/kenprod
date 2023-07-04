5aa09cdee5ba5191a2c45d4e25ca84008069107f
Simon <simon@kenpoly.com>
2023-07-04 19:11:46 +0300

    db settings switched to postgres

--- intel/settings.py
+++ intel/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','13.48.1.224']
+ALLOWED_HOSTS = ['*','16.171.24.147']
 
 
 # Application definition
@@ -86,23 +86,23 @@ WSGI_APPLICATION = 'intel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'dbtest2', 
-#         'USER': 'postgres',
-#         'PASSWORD': 'postgres',
-#         'HOST': 'localhost', 
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbprod', 
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost', 
+        'PORT': 5432,
+    }
+}
 
 # DATABASES = {
 #     'default': {
