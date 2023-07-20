651adf0a3d2d0aa2c0880106a2243418fa79970f
Simon <simon@kenpoly.com>
2023-07-20 14:11:36 +0300

    update

--- intel/settings.py
+++ intel/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','16.170.205.28']
+ALLOWED_HOSTS = ['*','localhost']
 
 
 # Application definition
