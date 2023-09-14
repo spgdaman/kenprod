4b47ada09c893b9106a1068322d0fb14f9edfbbd
Simon <simon@kenpoly.com>
2023-09-14 11:37:45 +0300

    icontains added to search parameters

--- price_app/filters.py
+++ price_app/filters.py
@@ -3,16 +3,14 @@ import django_filters
 from django import forms
 
 class MarketPriceFilter(django_filters.FilterSet):
-    created_at = django_filters.DateFromToRangeFilter(
-        # lookup_expr='icontains',
-        # widget= forms.DateInput(
-        #     attrs={
-        #         'class': 'datepicker'
-        #     }
-        # )
-    )
-    # created_at_gt = django_filters.DateFromToRangeFilter(field_name='created_at', lookup_expr='gt')
-    # created_at_lt = django_filters.DateFromToRangeFilter(field_name='created_at', lookup_expr='lt')
+    created_at = django_filters.DateFromToRangeFilter()
+    sales_person = django_filters.CharFilter(lookup_expr='icontains')
+    customer_name = django_filters.CharFilter(lookup_expr='icontains')
+    customer_branch = django_filters.CharFilter(lookup_expr='icontains')
+    kenpoly_product_name = django_filters.CharFilter(lookup_expr='icontains')
+    competitor_name = django_filters.CharFilter(lookup_expr='icontains')
+    competitor_product_name = django_filters.CharFilter(lookup_expr='icontains')
+    
     class Meta:
         model = MarketPrice
         fields = ['sales_person', 'customer_name', 'customer_branch', 'kenpoly_product_name', 'competitor_name', 'competitor_product_name', 'created_at']
